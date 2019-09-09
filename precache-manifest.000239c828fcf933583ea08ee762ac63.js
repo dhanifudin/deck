412958dc802f87cf755860a76792eb845b70cbcf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-09 09:59:45 +0000

    Deploy deck.dhanifudin.com to github.com/dhanifudin/deck.git:gh-pages

---
+++ precache-manifest.000239c828fcf933583ea08ee762ac63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7775f3aa1bebc8e54e99",
+    "url": "/static/js/main.7775f3aa.chunk.js"
+  },
+  {
+    "revision": "3886605d17169fa57a1e",
+    "url": "/static/js/1.3886605d.chunk.js"
+  },
+  {
+    "revision": "7775f3aa1bebc8e54e99",
+    "url": "/static/css/main.6ae52ec6.chunk.css"
+  },
+  {
+    "revision": "3886605d17169fa57a1e",
+    "url": "/static/css/1.08bb2bc1.chunk.css"
+  },
+  {
+    "revision": "2bddeaf505e430c09521f5e941eb2cb0",
+    "url": "/index.html"
+  }
+];
